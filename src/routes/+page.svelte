809e03a5ec50cf556184cf165d0c6e8e0c04a4bf
Ysn4Irix <80527916+Ysn4Irix@users.noreply.github.com>
2022-10-27 22:57:36 +0100

    :fire: fixing user-agent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,8 @@
 				url: vars.url
 			}),
 			headers: {
+				'User-Agent':
+					'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.9999.0 Safari/537.36',
 				'Content-Type': 'application/json'
 			}
 		}
